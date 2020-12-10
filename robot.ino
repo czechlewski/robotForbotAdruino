d28a8206c52375cd5ce039c26cb5585f4c7f2eee
czechlewskiUG <mczechlewski@inf.ug.edu.pl>
2020-12-10 13:40:05 +0100

    Configuration added

--- robot.ino
+++ robot.ino
@@ -1,4 +1,23 @@
+ #define LED 13
+ #define BUZZER 10
+ #define BUTTON 2
 void setup() {
+    //Konfiguracja wyjsc
+  pinMode(LED, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  //Konfiguracja wyjscia
+  pinMode(BUTTON, INPUT_PULLUP); 
+ 
+  //Wylaczenie diody i buzzera
+  digitalWrite(LED, LOW);
+  digitalWrite(BUZZER, LOW);
+ 
+  //Testowe migniecia LED
+  digitalWrite(LED, HIGH);
+  delay(500);
+  digitalWrite(LED, LOW);
+  delay(500);
+}
 }
 void loop() {
 }
