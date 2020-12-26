c91d7fe9f55a464715b0950bf86126836786d223
czechlewski <mikolaj.czechlewski@gmail.com>
2020-12-26 15:54:49 +0100

    Initial configuration

--- robot.ino
+++ robot.ino
@@ -1,4 +1,13 @@
+#define L_PWM 5
+#define L_DIR 4
+#define R_PWM 6
+#define R_DIR 9
 void setup() {
 }
+//Konfiguracja pinow od mostka H
+  pinMode(L_DIR, OUTPUT);
+  pinMode(R_DIR, OUTPUT);
+  pinMode(L_PWM, OUTPUT);
+  pinMode(R_PWM, OUTPUT);
 void loop() {
 }
